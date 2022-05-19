04b2649bc4d08493d0d2a9d9522825916cb21378
sabertooth9 <2013nuhash@gmail.com>
2022-05-19 19:49:21 +0600

    gradient add failed

--- KGS Employee/Extension.swift
+++ KGS Employee/Extension.swift
@@ -49,3 +49,20 @@ extension UIButton {
         layer.borderColor = UIColor.white.cgColor
     }
 }
+
+extension UIView{
+    // For insert layer in Foreground
+    func addBlackGradientLayerInForeground(frame: CGRect, colors:[UIColor]){
+        let gradient = CAGradientLayer()
+        gradient.frame = frame
+        gradient.colors = colors.map{$0.cgColor}
+        self.layer.addSublayer(gradient)
+    }
+    // For insert layer in background
+    func addBlackGradientLayerInBackground(frame: CGRect, colors:[UIColor]){
+        let gradient = CAGradientLayer()
+        gradient.frame = frame
+        gradient.colors = colors.map{$0.cgColor}
+        self.layer.insertSublayer(gradient, at: 0)
+    }
+}
--- KGS Employee/LoginViewController.swift
+++ KGS Employee/LoginViewController.swift
@@ -13,6 +13,7 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var textFieldUserName: UITextField!
     @IBOutlet weak var textFieldPassword: UITextField!
     @IBOutlet weak var buttonLogin: UIButton!
+    @IBOutlet weak var imageViewBack: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,6 +29,17 @@ class LoginViewController: UIViewController {
         viewForm.dropShadow(color: .lightGray, opacity: 0.5, offset: CGSize(width: 1, height: -1), radius: 20, scale: false)
         
         buttonLogin.createRoundButton()
+        
+        
+        let startColor = UIColor(red: 59, green: 129, blue: 47, alpha: 1)
+        let endColor = UIColor(red: 159, green: 197, blue: 38, alpha: 1)
+        
+//        gradient.colors = [startColor , endColor]
+//        gradient.locations = [0.0 , 1.0]
+//        tempView.layer.addSublayer(gradient)
+        
+        imageViewBack.addBlackGradientLayerInForeground(frame: imageViewBack.bounds, colors: [startColor , endColor])
+        
     }
 
 }
